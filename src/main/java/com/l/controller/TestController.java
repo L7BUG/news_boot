a4915ec413a9171bc2486be61d86cffc1c95b24b
manjaro-L <1411205284@qq.com>
2020-11-26 18:29:26 +0800

    测试Cross 跨域问题

--- src/main/java/com/l/controller/TestController.java
+++ src/main/java/com/l/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.l.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:8080")
 public class TestController {
     @PostMapping("/api/test")
-    public String test(@RequestBody JSONObject jsonObject){
-        System.out.println(jsonObject);
+    public String test(@RequestBody JSONObject body) {
+        System.out.println(body.getClass());
         return "{a: 123, b: 321}";
     }
 }
