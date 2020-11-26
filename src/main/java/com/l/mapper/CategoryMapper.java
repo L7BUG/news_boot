4a5873b5045994d4ca6bb060e514a0fc62fe2207
manjaro-L <1411205284@qq.com>
2020-11-26 14:09:51 +0800

    CategoryMapper 基本接口

--- src/main/java/com/l/mapper/CategoryMapper.java
+++ src/main/java/com/l/mapper/CategoryMapper.java
@@ -4,10 +4,45 @@ import com.l.pojo.Category;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface CategoryMapper {
+    /**
+     * 根据 主键查询单个
+     *
+     * @param integer
+     * @return
+     */
     Category selectByPrimaryKey(Integer integer);
 
-    Category selectByPrimaryKeyOrNews(Integer integer);
+    /**
+     * 查询所有
+     *
+     * @return
+     */
+    List<Category> selectAll();
+
+    /**
+     * 插入一条数据 返回 id 给 category
+     *
+     * @param category
+     * @return
+     */
+    int insertOne(Category category);
+
+    /**
+     * 根据主键删除一条信息
+     *
+     * @return
+     */
+    int deleteByPrimaryKey(Integer id);
+
+    /**
+     * 根据主键更新一条数据
+     *
+     * @return
+     */
+    int updateByPrimaryKey(Category category);
 }
--- src/test/java/com/l/NewsBootApplicationTests.java
+++ src/test/java/com/l/NewsBootApplicationTests.java
@@ -1,26 +1,26 @@
 package com.l;
 
-import com.l.mapper.NewMapper;
-import com.l.pojo.New;
+import com.l.mapper.CategoryMapper;
+import com.l.pojo.Category;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
 @SpringBootTest
 class NewsBootApplicationTests {
     @Autowired
-    NewMapper mapper;
+    CategoryMapper mapper;
 
     @Test
     void contextLoads() {
-        List<New> o = mapper.selectByPrimaryKey(1);
-        for (New i : o) {
-            System.out.println(i.getCategory());
-            System.out.println(i.getUser());
-            System.out.println(i);
-        }
-
+        System.out.println(mapper.selectAll());
+        System.out.println(mapper.selectByPrimaryKey(1));
+        Category category = new Category();
+        category.setName("测试栏目");
+        mapper.insertOne(category);
+        System.out.println(category);
+        category.setName("测试更新数据");
+        System.out.println(mapper.updateByPrimaryKey(category));
+        System.out.println(mapper.deleteByPrimaryKey(category.getId()));
     }
 }
