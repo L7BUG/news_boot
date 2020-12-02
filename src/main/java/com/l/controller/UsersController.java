b80339c6ee6297893a22852cf065c0e361e0a7cf
manjaro-L <1411205284@qq.com>
2020-12-02 14:51:37 +0800

    批量删除用户与删除单个用户

--- src/main/java/com/l/controller/UsersController.java
+++ src/main/java/com/l/controller/UsersController.java
@@ -1,8 +1,10 @@
 package com.l.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.l.json.CODE;
 import com.l.json.MyJSON;
 import com.l.pojo.User;
+import com.l.requestUrl.DELETE;
 import com.l.requestUrl.POST;
 import com.l.service.UserService;
 import com.l.utils.RespJsonUtils;
@@ -41,5 +43,58 @@ public class UsersController {
         } finally {
             return resp;
         }
+    }
+
+    /**
+     * 删除单个
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping(DELETE.DELETE_USER + "/{id}")
+    public MyJSON<Void> deleteOne(@PathVariable int id) {
+        MyJSON<Void> respData = null;
+        try {
+            User user = new User();
+            user.setId(id);
+            userService.deleteBySelective(user);
+            respData = RespJsonUtils.get(CODE.OK, "删除成功", null);
+        } catch (Exception e) {
+            respData = RespJsonUtils.get(CODE.ERROR, "因为约束关系无法删除！处理约束关系后继续！", null);
+        } finally {
+            return respData;
+        }
+    }
+
+    /**
+     * 删除多个用户
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping(DELETE.DELETE_USER)
+    public MyJSON<Void> deleteArr(@RequestBody List<User> request) {
+        StringBuffer sb = new StringBuffer();
+        boolean flag = true;
+        for (User user : request) {
+            try {
+                userService.deleteBySelective(user);
+                sb.append(user.getId() + "<p>成功</p>");
+                flag = false;
+            } catch (Exception e) {
+                sb.append(user.getId() + "<p>失败</p>");
+            }
+        }
+//        一个也没删除
+        if (flag) {
+            return RespJsonUtils.get(CODE.ERROR, "<h4>删除失败!请处理相关约束</h4>" + sb.toString(), null);
+        }
+//        全部删除完毕
+        if (sb.equals("")) {
+            return RespJsonUtils.get(CODE.OK, "<h4>删除成功</h4>", null);
+        }
+//        删除部分
+        return RespJsonUtils.get(CODE.OK, "<h4>部分资源因为约束关系删除失败！</h4>"
+                + sb.toString() + "<h4>请处理约束关系</h4>", null);
     }
 }
