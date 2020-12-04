31260b42f59a9c23f44373b2c45dcc331a220725
manjaro-L <1411205284@qq.com>
2020-12-04 13:46:00 +0800

    newMapper 接口添加分页查询 和 统计总数

--- src/test/java/com/l/NewsBootApplicationTests.java
+++ src/test/java/com/l/NewsBootApplicationTests.java
@@ -1,18 +1,33 @@
 package com.l;
 
-import com.l.mapper.CommentMapper;
+import com.l.mapper.NewMapper;
+import com.l.pojo.Category;
+import com.l.pojo.New;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class NewsBootApplicationTests {
     @Autowired
-    CommentMapper mapper;
+    NewMapper mapper;
 
     @Test
     void contextLoads() {
-        System.out.println(mapper.selectByPrimaryKey(1));
+        New n = new New();
+        Category category = new Category();
+        category.setId(1);
+        n.setCategory(category);
+        long l = mapper.selectSelectiveCount(n);
+        System.out.println(l);
+        for (long i = 1; i <= l / 5 + 1; i++) {
+            List<New> news = mapper.selectLimitBySelective(n, (int) ((i - 1) * 5), 5);
+            for (New j : news) {
+                System.out.println(j);
+            }
+        }
     }
 
 }
