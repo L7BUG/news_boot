0358267689565b66852d819b3fdf509ea046a6c7
L <1411205284@qq.com>
2020-12-12 21:56:00 +0800

    增加了接口的规范

--- src/main/java/com/l/json/CODE.java
+++ src/main/java/com/l/json/CODE.java
@@ -3,4 +3,5 @@ package com.l.json;
 public interface CODE {
         Integer OK = 200;
         Integer ERROR = 400;
+        Integer SERVER_ERROR = 500;
 }
--- src/main/java/com/l/json/MESSAGE.java
+++ src/main/java/com/l/json/MESSAGE.java
@@ -1,6 +1,7 @@
 package com.l.json;
 
 public interface MESSAGE {
-        String OK = "成功";
-        String ERROR = "错误";
-    }
+    String OK = "成功";
+    String ERROR = "错误";
+    String SERVER_ERROR = "服务器内部错误";
+}
