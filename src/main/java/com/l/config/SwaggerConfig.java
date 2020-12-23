88f5574c4bf2ca66504b582b325ed0dda80957ef
L7-BAG <ll789@gmail.com>
2020-12-23 13:12:21 +0800

    swagger 文档

---
+++ src/main/java/com/l/config/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.l.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+/**
+ * 开启swagger
+ *
+ * @author l
+ */
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("ByaoH")
+                .select()
+//                扫描什么路径
+                .apis(RequestHandlerSelectors.basePackage("com.l.controller"))
+//                过滤什么路径
+//                .paths(PathSelectors.ant("/l/**"))
+                .build();
+    }
+
+    @Bean
+    public Docket docket1() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("后台API");
+    }
+
+    private ApiInfo apiInfo() {
+//        作者信息
+        Contact contact = new Contact(
+                "陆梁雨",
+                "https://byaoh.coding.net/public/",
+                "ll789y@gmail.com");
+        ApiInfo apiInfo = new ApiInfo(
+                "老陆新闻头条后台管理系统API",
+                "API文档",
+                "1.0",
+                "urn:tos",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+        return apiInfo;
+    }
+}
